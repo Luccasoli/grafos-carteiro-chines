f4479ab3e74aa806a5dec933bb5801ffde788f04
luccasoli <lucasoliveira98@outlook.com>
2018-06-26 20:33:44 -0300

    Adicionado: Funcao principal para testes

---
+++ main.py
@@ -0,0 +1,20 @@
+from Graph import Graph
+#3 4 2 3 0 1 1 
+def main():
+	g = Graph()	
+
+	g.addEdge(2, 5)
+	g.addEdge(2, 4)
+	g.addEdge(2, 1)
+	g.addEdge(4, 3)
+
+	#g.rmEdge(1, 2)
+	print("Seu grafo:\n{}".format(g.graph))	
+	print("DFS:")
+	g.DFS(2)
+	print("BFS:")
+	g.BFS(2)
+
+
+if __name__ == '__main__':
+	main()
